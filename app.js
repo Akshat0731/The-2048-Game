e3bf1b2ccb970f8fe5c02b8dff3f05171d930a5f
Akshat0731 <akshatchauhan.co23d2@scet.ac.in>
2026-08-17 20:28:34 +0530

    added initial files

---
+++ app.js
@@ -0,0 +1,11 @@
+let btns = document.querySelectorAll('.tiles');
+
+// window.addEventListener("keydown",(e)=>{
+//     console.dir(e);
+// })
+
+
+function randomTileGenerator(){
+    let tileOne = Math.floor(Math.random()*15)+1;
+    let tileTwo = Math.floor(Math.random()*15)+1;
+}
